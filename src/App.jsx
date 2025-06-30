15c975df54a6e4c2eb485f51ba005b80e2fdf949
Birajit Saikia <birajitsaikia@gmail.com>
2025-06-30 13:14:38 +0530

    feat:add code of conduct page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Layout from './components/layout';
 import Home from './Home';
 import RoadmapPage from './pages/project/roadmap';
 import DownloadPage from './pages/download';
+import ConductPage from './pages/project/conduct';
+
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route index element={<Home />} />
         <Route path="project/roadmap" element={<RoadmapPage />} />
         <Route path="download" element={<DownloadPage />} />
+        <Route path="events" element={<EventsIndex />} />
+        <Route path="project/conduct" element={<ConductPage />} />
       </Route>
     </Routes>
   );
