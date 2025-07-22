2128dc16be56a4ee22a7afbe2d001f63eb1798df
Birajit Saikia <birajitsaikia@gmail.com>
2025-07-22 07:47:47 +0530

    feat: add `events` page and its sub-pages (#9)
    * feat: add jam events page
    
    * feat: add events page and all its sub-pages
    
    * Apply suggestions from code review
    
    Co-authored-by: Kris Stern <krisstern@outlook.com>
    
    * Update src/pages/events/contributor-summit.jsx
    
    Co-authored-by: Kris Stern <krisstern@outlook.com>
    
    * Update src/pages/events/event-kit.jsx
    
    Co-authored-by: Kris Stern <krisstern@outlook.com>
    
    ---------
    
    Co-authored-by: Kris Stern <krisstern@outlook.com>

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import Layout from './components/layout';
 import Home from './Home';
 import RoadmapPage from './pages/project/roadmap';
 import DownloadPage from './pages/download';
+import EventsIndex from './pages/events/index';
+import OnlineMeetup from './pages/events/online-meetup';
+import ContributorSummit from './pages/events/contributor-summit';
+import DevOpsWorld from './pages/events/devops-world';
+import Hacktoberfest from './pages/events/hacktoberfest';
+import HacktoberfestFaq from './pages/events/faq';
+import HacktoberfestEventKit from './pages/events/event-kit';
+import JamPage from './pages/projects/jam';
 
 function App() {
   return (
@@ -11,6 +19,14 @@ function App() {
         <Route index element={<Home />} />
         <Route path="project/roadmap" element={<RoadmapPage />} />
         <Route path="download" element={<DownloadPage />} />
+        <Route path="events" element={<EventsIndex />} />
+        <Route path="events/online-meetup" element={<OnlineMeetup />} />
+        <Route path="events/contributor-summit" element={<ContributorSummit />} />
+        <Route path="events/devops-world" element={<DevOpsWorld />} />
+        <Route path="events/hacktoberfest" element={<Hacktoberfest />} />
+        <Route path="events/hacktoberfest/faq" element={<HacktoberfestFaq />} />
+        <Route path="events/hacktoberfest/event-kit" element={<HacktoberfestEventKit />} />
+        <Route path="projects/jam" element={<JamPage />} />
       </Route>
     </Routes>
   );
